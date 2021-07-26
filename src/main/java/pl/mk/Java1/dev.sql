a0acc0193e97e9b40737e770cd774adaa4836315
MazieKor <maciej.coders@gmail.com>
2021-07-26 23:29:16 +0200

    Testing SQL - new Database, new table categories

---
+++ src/main/java/pl/mk/Java1/dev.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE devExpress CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE TABLE categories (CategoryID int NOT NULL AUTO_INCREMENT, CategoryName varchar(255) NOT NULL,
+CategoryDescription varchar(255),PRIMARY KEY (CategoryID));
