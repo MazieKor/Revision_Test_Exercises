f7605c123f2a916e5eb59967fecaa3bf2a817d31
Maciej Korczala <maciej.coders@gmail.com>
2021-07-26 23:50:03 +0200

    adding new table products

--- src/main/java/pl/mk/Java1/dev.sql
+++ src/main/java/pl/mk/Java1/dev.sql
@@ -1,3 +1,15 @@
 CREATE DATABASE devExpress CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE TABLE categories (CategoryID int NOT NULL AUTO_INCREMENT, CategoryName varchar(255) NOT NULL,
 CategoryDescription varchar(255),PRIMARY KEY (CategoryID));
+
+CREATE TABLE products (ProductID int NOT NULL AUTO_INCREMENT, ProductName varchar(255) NOT NULL,
+QuantityPerUnit float(10,2), UnitPrice decimal (10,2), UnitsInStock int unsigned,
+UnitsOnOrder double (10,2), Discontinued BOOLEAN, CategoryID int NOT NULL,
+PRIMARY KEY (ProductID), FOREIGN KEY(CategoryID) REFERENCES categories(CategoryID));
+
+describe categories;
+describe products;
+
+
+
+
