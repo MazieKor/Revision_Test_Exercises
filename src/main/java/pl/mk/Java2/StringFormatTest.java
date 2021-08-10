faf7bf9e5e649ffaef1854a6865a1617a372876f
Maciej Korczala <maciej.coders@gmail.com>
2021-08-10 23:44:43 +0200

    testing String.format - some additional tests

--- src/main/java/pl/mk/Java2/StringFormatTest.java
+++ src/main/java/pl/mk/Java2/StringFormatTest.java
@@ -15,7 +15,25 @@ public class StringFormatTest {
         String result3 = String.format(test3, "12");    //can't be decimal
         System.out.println(result3);
 
+        String test4 = "My answer is %h !";
+        String result4 = String.format(test4, "rdt");    //gives hexadecimal 1b882
+        System.out.println(result4);
 
+        String test5 = "My answer is %c !";
+        String result5 = String.format(test5, 56);     //gives 8
+        System.out.println(result5);
+
+        String test6 = "My answer %n is %f !";
+        String result6 = String.format(test6, 17688.9899999d);     //gives automatically 8 decimal places + next line
+        System.out.println(result6);
+
+        String test7 = "My answer %nis %% %.4s !";
+        String result7 = String.format(test7, "NOWY ARG");     //limits signs in string
+        System.out.println(result7);
+
+        String test8 = "My answer is %f and %f !";
+        String result8 = String.format(test8, 142d, 99d);     //without direct pointing an argument ($)
+        System.out.println(result8);
 
     }
 }
