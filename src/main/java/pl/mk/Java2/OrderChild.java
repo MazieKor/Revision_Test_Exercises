3eac434f4a80af5c9cd7ba20d3751c4855d75b74
Maciej Korczala <maciej.coders@gmail.com>
2021-08-18 23:32:47 +0200

    testing order of initialization with static and instance fields and methods + constructors

--- src/main/java/pl/mk/Java2/OrderChild.java
+++ src/main/java/pl/mk/Java2/OrderChild.java
@@ -1,17 +1,34 @@
 package pl.mk.Java2;
 
 public class OrderChild extends OrderParent{
+    static String staticParentName = "staticChildName";
+    static {
+        System.out.println(staticParentName);
+    }
+    static String staticChildCity = "Gliwice";
+    static {
+        System.out.println(staticChildCity);
+    }
+
     int test;
+    int test2 = 222;
+    int test3 = 333;
 
     OrderChild() {
         methodTest();
         test = 7;
+        test2 = 222222222;
         methodTest();
         System.out.println("Child Constr");
     }
 
     void methodTest() {
         System.out.println("Method, field is " + test);
+        System.out.println("Method, field2 is " + test2);
+    }
+
+    void methodTes2() {
+        System.out.println("Method2, field2 is " + test2);
     }
 
     public static void main(String[] args) {
--- src/main/java/pl/mk/Java2/OrderParent.java
+++ src/main/java/pl/mk/Java2/OrderParent.java
@@ -1,10 +1,32 @@
 package pl.mk.Java2;
 
 public class OrderParent {
-
-
-    OrderParent() {
-        System.out.println("Parent Constr");
+    String parentName = "parentName";
+    {
+        System.out.println(parentName);
+    }
+    static  String parentCity = "Zabrze";
+    static {
+        System.out.println(parentCity);
     }
 
+    static String staticParentName = "staticParentName";
+    static {
+        System.out.println(staticParentName);
+    }
+    int test2 = 2323;
+
+    OrderParent() {
+        System.out.println("Parent Field2 is: " + test2);
+        System.out.println("Parent Constr");
+        staticParentName = "staticParent name in Constructor";
+        System.out.println(staticParentName);
+        methodTes2();
+
+
+    }
+
+    void methodTes2() {
+        System.out.println("Method2 Parent, field2 is " + test2);
+    }
 }
